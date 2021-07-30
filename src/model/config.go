ec756da77ea4cb8a438728a454daaf1307388bef
cheolgyu <rb7480@knou.ac.kr>
2021-07-30 11:34:43 +0900

    clean-arch : remove old file

--- src/model/config.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Config struct {
-	Id         int    `json:"id"`
-	Upper_code string `json:"upper_code"`
-	Upper_name string `json:"upper_name"`
-	Code       string `json:"code"`
-	Name       string `json:"name"`
-}
