48cc8d186daf152aa1847fb47326efabb3ffdc7b
cheolgyu <rb7480@knou.ac.kr>
2022-01-27 15:36:34 +0900

    copy

---
+++ src/svc/52weeks/handler.go
@@ -0,0 +1,64 @@
+package project
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/cheolgyu/stock-read-pub-api/src/domain"
+	"github.com/julienschmidt/httprouter"
+)
+
+type Handler struct {
+	usecase domain.ProjectUsecase
+}
+
+func NewHandler(r *httprouter.Router, obj_usecase domain.ProjectUsecase) {
+
+	h := Handler{usecase: obj_usecase}
+	//chk := CheckHandler{}
+
+	r.GET("/project/day_trading", h.GetDayTradingByPaging)
+	r.GET("/project/monthly_peek", h.GetMonthlyPeekByPaging)
+
+}
+
+func (obj *Handler) GetDayTradingByPaging(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	req_id := r.Header.Get("req_id")
+
+	////////////////////////////////////////////////////////
+	q := r.URL.Query()
+	log.Printf("<%s>  params=%s \n", req_id, q)
+	paging := domain.ProjectParamsString{}
+	paging.Set(q)
+	////////////////////////////////////////////////////////
+
+	//ctx := r.Context()
+	cmp, err := obj.usecase.GetDayTradingByPaging(context.TODO(), paging)
+	if err != nil {
+		log.Println(err)
+	}
+	json.NewEncoder(w).Encode(cmp)
+	log.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", req_id)
+
+}
+
+func (obj *Handler) GetMonthlyPeekByPaging(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	req_id := r.Header.Get("req_id")
+
+	///////////////////////////////////////////////////
+	q := r.URL.Query()
+	log.Printf("<%s>  params=%s \n", req_id, q)
+	paging := domain.ProjectParamsString{}
+	paging.Set(q)
+	///////////////////////////////////////////////////
+
+	cmp, err := obj.usecase.GetMonthlyPeekByPaging(context.TODO(), paging)
+	if err != nil {
+		log.Println(err)
+	}
+	json.NewEncoder(w).Encode(cmp)
+	log.Println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", req_id)
+
+}
---
+++ src/svc/52weeks/repository.go
@@ -0,0 +1,114 @@
+package project
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/cheolgyu/stock-read-pub-api/src/domain"
+	"github.com/cheolgyu/stock-read-pub-api/src/domain/utils"
+	"github.com/jmoiron/sqlx"
+)
+
+type ProjectRepository struct {
+	conn *sqlx.DB
+}
+
+func NewRepository(Conn *sqlx.DB) domain.ProjectRepository {
+
+	return &ProjectRepository{conn: Conn}
+}
+
+func (obj *ProjectRepository) GetDayTradingByPaging(ctx context.Context, params domain.ProjectParams) ([]map[string]interface{}, error) {
+
+	q := "select * from project.func_day_trading(%v, %v, %v, '%s', '%s', ARRAY[ %s])"
+
+	m_str := ""
+	for i, v := range params.Market {
+		m_str += fmt.Sprintf("%v", v)
+		if i+1 < len(params.Market) {
+			m_str += ","
+		}
+	}
+	query := fmt.Sprintf(q, params.Term, params.Paging.Limit, params.Paging.Offset, params.Paging.Sort, params.Paging.Desc, m_str)
+
+	log.Printf("query=%s \n", query)
+
+	var rows *sqlx.Rows
+	var err error
+	rows, err = obj.conn.Queryx(query)
+
+	if err != nil {
+		log.Printf("GetDayTradingByPaging:Queryx::error::::<%s> query= \n", q)
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	var list []map[string]interface{}
+
+	for rows.Next() {
+		item := make(map[string]interface{})
+
+		err = rows.MapScan(item)
+
+		if err != nil {
+			panic(err)
+		}
+		list = append(list, utils.Decode(item))
+	}
+
+	return list, err
+}
+
+func (obj *ProjectRepository) GetMonthlyPeekByPaging(ctx context.Context, params domain.ProjectParams) ([]map[string]interface{}, error) {
+	log.Println("~~~~~~~ProjectRepository~~~~~~GetMonthlyPeekByPaging~~~~~~~~~~~~~~~~~~~~~~~")
+	q := `
+SELECT *
+from PUBLIC.view_project_trading_volume
+WHERE 1 = 1 
+	and unit_type=  ` + fmt.Sprintf("%v", params.UnitType) + `
+	`
+	if params.UnitVal > 0 {
+		q += `and  max_unit =   ` + fmt.Sprintf("%v", params.UnitVal)
+	}
+	if len(params.Market) > 0 {
+		q += `and market_type in ( `
+		for i, v := range params.Market {
+			if i > 0 {
+				q += ` ,`
+			}
+			q += ` ` + fmt.Sprintf("%v", v) + ` `
+		}
+
+		q += ` ) `
+	}
+	if params.Paging.Sort != "" {
+		q += ` order by  ` + params.Paging.Sort + `  ` + params.Paging.Desc + ` `
+	}
+	q += `limit ` + strconv.Itoa(params.Paging.Limit) + ` OFFSET ` + strconv.Itoa(params.Paging.Offset)
+	log.Printf("query=%s \n", q)
+	rows, err := obj.conn.Queryx(q)
+	if err != nil {
+		log.Printf("GetMonthlyPeekByPaging:Queryx::error::::<%s> query= \n", q)
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	var list []map[string]interface{}
+
+	for rows.Next() {
+		item := make(map[string]interface{})
+
+		err = rows.MapScan(item)
+
+		if err != nil {
+			panic(err)
+		}
+		list = append(list, utils.Decode(item))
+	}
+
+	return list, err
+}
